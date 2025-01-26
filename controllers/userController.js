558cfc030ad788e14bc50f2ca507281509f85c3a
mohammed alsamaki <samkimohd@gmail.com>
2025-01-26 12:10:31 +0300

    Save progr

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,4 @@
+const { default: mongoose } = require('mongoose');
 const User = require('../models/userModel');
 //  get method to get all users
 const getallUsers = async (req ,res)=>{
@@ -38,5 +39,28 @@ const findUserByRole = async (req,res)=>{
         res.status(500).json({message:error});
     }
 }
-module.exports= {getallUsers,createNewUser,findUserByRole};
+//  add function to delete user by id using params 
+const deleteUserByID = async (req,res)=>{
+    try {
+        const {id} =req.params;
+        console.log('this is the id',id);
+        const deleteUser = await User.findByIdAndDelete(id);
+        console.log(mongoose.Types.ObjectId.isValid(id))
+        if(!mongoose.Types.ObjectId.isValid(id)){
+            return res.status(404).send('id not valid');
+        }
+        if(!deleteUser){
+            res.status(404).json({message:"User not found"});
+        }
+        else{
+            res.status(200).json({message:"User deleted successfully"});
+        }
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message:error});
+        
+    }
+}
+module.exports= {getallUsers,createNewUser,findUserByRole,deleteUserByID};
 
--- routers/userRouters.js
+++ routers/userRouters.js
@@ -1,11 +1,12 @@
 
 const express  = require('express');
 const router = express.Router();
-const {getallUsers,createNewUser,findUserByRole} = require('../controllers/userController');
+const {getallUsers,createNewUser,findUserByRole,deleteUserByID} = require('../controllers/userController');
 
 router.get('/users', getallUsers);
 router.post('/users',createNewUser);
 router.post('/users/role',findUserByRole);
+router.delete('/deleteUSer/:id',deleteUserByID);
 
 
 module.exports = router;
